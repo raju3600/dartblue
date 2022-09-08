fe3defe3eef359986a2f285ec82262eb2ccefbfc
raju3600 <raju360.n@gmail.com>
2022-09-08 11:55:49 +0530

    another commit in teja.sh

--- teja.sh
+++ teja.sh
@@ -64,12 +64,13 @@ if [ $? -eq 0 ]; then
        
     fi
 fi
-#for branch in $(git show-ref --heads | sed 's/.*refs\/heads\///');
 
-#do 
+for branch in $(git show-ref --heads | sed 's/.*refs\/heads\///');
+
+do 
 	
 
-#	git push origin $(branch) 
-#done
+	git push origin $(branch) 
+done
 
 
